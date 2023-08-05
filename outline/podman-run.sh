13cb578e875e50835a8ee6eeea41b0abd216cc0f
Tomi Ollila <tomi.ollila@iki.fi>
2023-08-05 12:53:31 +0300

    podman-run.sh updates
    - xdg and dbus settings also when WAYLAND_DISPLAY not set
    - 'ff' as hostname
    - podman images list format so that image name can be copy-pasted
    - [options] before image name does not work, so removed from usage

--- outline/podman-run.sh
+++ outline/podman-run.sh
@@ -11,9 +11,11 @@ case ${ZSH_VERSION-} in *.*) emulate ksh; esac
 set -euf  # hint: (z|ba|da|'')sh -x thisfile [args] to trace execution
 
 test $# = 0 && {
-	podman images debian-12-fontforge
 	echo
-	echo "Usage: $0 [options] image [command [arg ...]]"
+	fmt='{{.Repository}}:{{.Tag}} - {{.Size}} - {{.CreatedSince}}'
+	podman images --format="$fmt" debian-12-fontforge
+	echo
+	echo "Usage: $0 image [command [arg ...]]"
 	echo
 	exit 1
 }
@@ -26,18 +28,18 @@ case $1 in *:*) ;; *)
 esac
 
 wo="-v /etc/localtime:/etc/localtime:ro --device /dev/dri"
+uid=${UID:-`id -u`}
+wo="$wo -v /run/user/$uid/bus:/tmp/bus"
+wo="$wo -e DBUS_SESSION_BUS_ADDRESS=unix:path=/tmp/bus"
+wo="$wo -e XDG_RUNTIME_DIR=/tmp"
+
 test "${DISPLAY-}" && {
 	wo="$wo -v /tmp/.X11-unix:/tmp/.X11-unix"
 	wo="$wo -e DISPLAY=$DISPLAY"
 }
 test "${WAYLAND_DISPLAY-}" && {
-	uid=${UID:-`id -u`}
-	wo="$wo -v /run/user/$uid/bus:/tmp/bus"
-	wo="$wo -e DBUS_SESSION_BUS_ADDRESS=unix:path=/tmp/bus"
-	wo="$wo -e XDG_RUNTIME_DIR=/tmp"
 	wo="$wo -v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/tmp/wayland-0"
 	wo="$wo -e WAYLAND_DISPLAY=/tmp/wayland-0"
-	unset uid
 }
 wo="$wo --security-opt label=type:container_runtime_t"
 
@@ -49,7 +51,7 @@ test $# -gt 0 && set -- sh -c 'exec "$0" "$@"' "$@"
 x_exec () { printf '+ %s\n' "$*" >&2; exec "$@"; }
 
 x_exec \
-podman run --pull=never --rm -it --privileged $wo \
+podman run --pull=never --rm -it --privileged $wo -h ff \
 	--tmpfs /tmp --tmpfs /run -v "$HOME:$HOME" -w "$PWD" $ci "$@"
 
 # Local variables:
